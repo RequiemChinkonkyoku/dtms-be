aceb20138ad9f0004078af46539653effb09a4bd
TienTran23 <trannhuttienvn23@gmail.com>
2024-12-25 10:42:31 +0700

    Add getAllDocumentType

---
+++ DTMS_API/Controllers/DogDocumentTypeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Implement;
+using Services.Interface;
+
+namespace DTMS_API.Controllers
+{
+    [ApiController]
+    [Route("api/dogDocumentTypes")]
+    public class DogDocumentTypeController : Controller
+    {
+        private readonly IDogDocumentTypeService _documentTypeService;
+        public DogDocumentTypeController(IDogDocumentTypeService documentTypeService)
+        {
+            _documentTypeService = documentTypeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllDocumentType()
+        {
+            var response = await _documentTypeService.GetAllDocumentType();
+            return Ok(response);
+        }
+    }
+}
--- Services/Implement/DogDocumentTypeService.cs
+++ Services/Implement/DogDocumentTypeService.cs
@@ -1,4 +1,6 @@
-﻿using Services.Interface;
+﻿using Models.Entities;
+using Repositories.Interface;
+using Services.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,16 @@ namespace Services.Implement
 {
     public class DogDocumentTypeService : IDogDocumentTypeService
     {
+        private readonly IUnitOfWork _unitOfWork ;
+        public DogDocumentTypeService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork ;
+        }
+
+        public async Task<List<DogDocumentType>> GetAllDocumentType()
+        {
+            var result = await _unitOfWork.DogDocumentTypes.GetAll();
+            return result;
+        }
     }
 }
