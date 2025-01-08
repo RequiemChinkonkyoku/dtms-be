92f4457e6275232638835ff753efbe7b65a2c3c3
RequiemRekuiemu <phuongdo0207@gmail.com>
2025-01-08 10:40:54 +0700

    BREAKING CHANGE(database): updated profile attributes

--- DTMS_API/Program.cs
+++ DTMS_API/Program.cs
@@ -1,6 +1,10 @@
+using System.Security.Claims;
+using System.Text;
 using AutoMapper;
 using DTMS_API.Extension;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 using Models.Automapper;
@@ -25,13 +29,84 @@ builder.Services.AddServiceExtension(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(opt =>
-    opt.MapType<DateOnly>(() => new OpenApiSchema
     {
-        Type = "string",
-        Format = "date",
-        Example = new OpenApiString(DateTime.Today.ToString("yyyy-MM-dd"))
-    })
-    );
+        opt.MapType<DateOnly>(() => new OpenApiSchema
+        {
+            Type = "string",
+            Format = "date",
+            Example = new OpenApiString(DateTime.Today.ToString("yyyy-MM-dd"))
+        });
+        opt.SwaggerDoc("v1", new OpenApiInfo { Title = "DTMS API", Version = "v1" });
+        opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
+    }
+);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "DTMS_Policy",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey
+            (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = true
+    };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("CustomerAuth",
+        policy => policy.RequireClaim(ClaimTypes.Role, "1"));
+    options.AddPolicy("TrainerAuth",
+        policy => policy.RequireClaim(ClaimTypes.Role, "2"));
+    options.AddPolicy("StaffAuth",
+        policy => policy.RequireClaim(ClaimTypes.Role, "3"));
+    options.AddPolicy("AdminAuth",
+        policy => policy.RequireClaim(ClaimTypes.Role, "4"));
+});
+
+builder.Services.AddDistributedMemoryCache();
 
 var app = builder.Build();
 
@@ -42,7 +117,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("DTMS_Policy");
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
