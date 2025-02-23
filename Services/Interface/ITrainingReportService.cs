e50e659cd2cb71d93e0c5c4138a9dc24cee28a96
Nguyễn Trọng Nghĩa <nguyentrongnghia2003@gmail.com>
2025-02-23 15:54:10 +0700

    fix dogId in create/update and change baseResponse for trainingReport

--- Services/Interface/ITrainingReportService.cs
+++ Services/Interface/ITrainingReportService.cs
@@ -1,14 +1,15 @@
-﻿using Models.DTOs.TrainingReport;
+﻿using Models.DTOs;
+using Models.DTOs.TrainingReport;
 
 namespace Services.Interface
 {
     public interface ITrainingReportService
     {
-        Task<List<TrainingReportResponse>> GetAllTrainingReports();
-        Task<TrainingReportResponse> GetTrainingReportById(string id);
-        Task<List<TrainingReportResponse>> GetTrainingReportsByDogId(string dogId);
-        Task<TrainingReportResponse> CreateTrainingReportAsync(CreateTrainingReportRequest createTrainingReportRequest);
-        Task<TrainingReportResponse> UpdateTrainingReportAsync(string id, CreateTrainingReportRequest request);
+        Task<BaseResponseDTO<TrainingReportResponse>> GetAllTrainingReports();
+        Task<BaseResponseDTO<TrainingReportResponse>> GetTrainingReportById(string id);
+        Task<BaseResponseDTO<TrainingReportResponse>> GetTrainingReportsByDogId(string dogId);
+        Task<BaseResponseDTO<TrainingReportResponse>> CreateTrainingReportAsync(CreateTrainingReportRequest createTrainingReportRequest);
+        Task<BaseResponseDTO<TrainingReportResponse>> UpdateTrainingReportAsync(string id, CreateTrainingReportRequest request);
         Task<bool> DeleteTrainingReportAsync(string id);
     }
 }
