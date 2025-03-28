0af29a009238dafeca10081d429ee405ff75a46e
TienTran23 <trannhuttienvn23@gmail.com>
2025-03-28 21:42:58 +0700

    get availabe Trainer

--- Repositories/Interface/IAccountRepository.cs
+++ Repositories/Interface/IAccountRepository.cs
@@ -1,8 +1,10 @@
+using Models.DTOs;
+using Models.DTOs.Response;
 using Models.Entities;
 
 namespace Repositories.Interface;
 
 public interface IAccountRepository : IRepositoryBase<Account>
 {
-    
+    Task<List<Account>> GetAvailableTrainersAsync(TrainerAvailabilityRequest request);
 }
